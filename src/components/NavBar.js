7dbb376890f1294854260f51c280f0dfae58ee38
Michael Okoe <michael.okoejr@gmail.com>
2022-07-26 15:06:30 +0000

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,18 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const newLinks = links.map(link => {
+    const hashString = "#" + link;
+    return  (<a key={link} href={hashString}>{link}</a>)
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+        {/* This returns the <a> of the array 'links*/}
+        {newLinks}
+    </nav>
+  )
+  
 }
 
 export default NavBar;
